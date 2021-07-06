748d1633121bd30b054ee315b72b14002826fdf5
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-07-06 09:56:24 +0200

    add state to dice

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ export default class Die extends Component {
     render() {
         return (
             <div className="dice">
-                <h1><i className="fas fa-dice-one"></i></h1>
+            <i className={`fas fa-dice-${this.props.face}`}></i>
             </div>
         )
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import Die from './Die'
 
 export default class RollDice extends Component {
+    constructor(props){
+        super(props);
+        this.state = {die1: "five", die2: "two"}
+    }
     render() {
         return (
             <div>
-                <Die />
-                <Die />
-                <button>Roll Dice!</button>
+                <section>
+                <Die face={this.state.die1}/>
+                <Die face={this.state.die2}/>
+                </section>
+                <button className="button">Roll Dice!</button>
                 
             </div>
         )
